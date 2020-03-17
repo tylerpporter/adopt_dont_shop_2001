b1fd23e5052b6ebe6ce5630a966c6b5498a2dd10
Tyler Porter <tylerpporter@gmail.com>
2020-03-17 15:13:31 -0600

    Pass test for showing individual shelters

---
+++ spec/features/shelters/shelters_index_page_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe "shelters index page", type: :feature do
+  it "can see all shelter names" do
+    shelter1 = Shelter.create(name: 'FuzzTime',
+                              address: "895 Fuzz St.",
+                              city: "Westminster",
+                              state: "CO",
+                              zip: "80021")
+    shelter2 = Shelter.create(name: 'DogPaws',
+                              address: "123 Pup St.",
+                              city: "Arvada",
+                              state: "CO",
+                              zip: "80005")
+    visit "/shelters"
+
+    expect(page).to have_content(shelter1.name)
+    expect(page).to have_content(shelter2.name)
+  end
+end
+
+RSpec.describe "shelters index page/:id", type: :feature do
+  it "can see a shelters info for given id" do
+    shelter1 = Shelter.create(name: 'FuzzTime',
+                              address: "895 Fuzz St.",
+                              city: "Westminster",
+                              state: "CO",
+                              zip: "80021")
+    shelter2 = Shelter.create(name: 'DogPaws',
+                              address: "123 Pup St.",
+                              city: "Arvada",
+                              state: "CO",
+                              zip: "80005")
+    visit "/shelters/#{shelter1.id}"
+
+    expect(page).to have_content(shelter1.name)
+    expect(page).to have_content(shelter1.address)
+    expect(page).to have_content(shelter1.city)
+    expect(page).to have_content(shelter1.state)
+    expect(page).to have_content(shelter1.zip)
+
+    visit "/shelters/#{shelter2.id}"
+
+    expect(page).to have_content(shelter2.name)
+    expect(page).to have_content(shelter2.address)
+    expect(page).to have_content(shelter2.city)
+    expect(page).to have_content(shelter2.state)
+    expect(page).to have_content(shelter2.zip)
+  end
+end

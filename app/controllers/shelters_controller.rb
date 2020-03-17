843055e7a148c46eca4e604754097e96ffcf376b
Tyler Porter <tylerpporter@gmail.com>
2020-03-16 22:38:42 -0600

    Complete user stories 1-4

---
+++ app/controllers/shelters_controller.rb
@@ -0,0 +1,26 @@
+class SheltersController < ApplicationController
+
+  def index
+    @shelters = Shelter.all
+  end
+
+  def new
+  end
+
+  def create
+    shelter =Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+      shelter.save
+      redirect_to '/shelters'
+  end
+
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
+
+end
